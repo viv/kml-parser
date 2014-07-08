e86c60db86465a687b71b640bc713129faca1d18
Matthew Vivian <matthew@postino.co.uk>
2014-07-08 18:23:59 +0100

    Open kmz, read kml and start to pull out info

---
+++ parser.php
@@ -0,0 +1,23 @@
+<?php
+$file = 'test/data/ride-one.kmz';
+
+$zip = new ZipArchive;
+
+if ($zip->open($file) === TRUE) {
+  $contents = $zip->getFromIndex(0);
+  if ($contents) {
+    $xml = simplexml_load_string($contents);
+    echo $xml->Document->name . PHP_EOL;
+
+    foreach ($xml->Document->Placemark as $placemark) {
+      echo $placemark->styleUrl . PHP_EOL;
+    }
+
+    $xml->registerXPathNamespace('kml', 'http://earth.google.com/kml/2.2');
+    var_dump($xml->xpath('//kml:description'));
+  }
+
+  $zip->close();
+} else {
+  echo "Failed to open {$file}";
+}
