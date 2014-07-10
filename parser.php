0108e48edc8e89508be2872fe5f17f9af0f4b2b3
Matthew Vivian <matthew@postino.co.uk>
2014-07-10 17:52:17 +0100

    Start pulling out stats from MotionX description

--- parser.php
+++ parser.php
@@ -1,28 +1,80 @@
 <?php
 $file = 'test/data/ride-one.kmz';
 
-$zip = new ZipArchive;
+$xml = getXml($file);
 
-if ($zip->open($file) === TRUE) {
-  $contents = $zip->getFromIndex(0);
-  if ($contents) {
-    $xml = simplexml_load_string($contents);
-    echo $xml->Document->name . PHP_EOL;
+echo getTrackName($xml) . PHP_EOL;
+echo getTrackDate($xml) . PHP_EOL;
+echo getDistance($xml) . PHP_EOL;
+echo getElapsedTime($xml) . PHP_EOL;
 
-    foreach ($xml->Document->Placemark as $placemark) {
-      if ($placemark->styleUrl == '#fpTrackStyle') {
-        foreach ($placemark->description->div->div->table->tr as $row) {
-          var_dump($row);
+function getXml($file) {
+    $zip = new ZipArchive;
+
+    if ($zip->open($file) === TRUE) {
+        $contents = $zip->getFromIndex(0);
+        $zip->close();
+        if ($contents) {
+            return simplexml_load_string($contents);
         }
-      }
-      echo $placemark->styleUrl . PHP_EOL;
+    } else {
+        echo "Failed to open {$file}";
     }
+}
 
+function getTrackName($xml) {
+    return $xml->Document->name;
+}
+
+function getTrackDescription($xml) {
     $xml->registerXPathNamespace('kml', 'http://earth.google.com/kml/2.2');
-    var_dump($xml->xpath('//kml:description'));
-  }
+    return $xml->xpath('//kml:description')[0];
+}
 
-  $zip->close();
-} else {
-  echo "Failed to open {$file}";
+function getTrackDate($xml) {
+    return strip_tags(getTrackDescription($xml)->div->div->table->tr->td[1]->asXML());
+}
+
+function getDistance($xml) {
+    return strip_tags(getTrackDescription($xml)->div->div->table->tr[1]->td[1]->asXML());
+}
+
+function getElapsedTime($xml) {
+    return strip_tags(getTrackDescription($xml)->div->div->table->tr[2]->td[1]->asXML());
+}
+
+function getMaxSpeed($xml) {
+    return $xml->Document->name;
+}
+
+function getAverageSpeed($xml) {
+    return $xml->Document->name;
+}
+
+function getAveragePace($xml) {
+    return $xml->Document->name;
+}
+
+function getMinAltitude($xml) {
+    return $xml->Document->name;
+}
+
+function getMaxAltitude($xml) {
+    return $xml->Document->name;
+}
+
+function getStartLocation($xml) {
+    return $xml->Document->name;
+}
+
+function getFinishLocation($xml) {
+    return $xml->Document->name;
+}
+
+function getStartTime($xml) {
+    return $xml->Document->name;
+}
+
+function getFinishTime($xml) {
+    return $xml->Document->name;
 }
