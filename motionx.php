c930c91753eb98ce6205392f8994fefc70ae0bda
Matthew Vivian <matthew@postino.co.uk>
2014-07-12 10:57:48 +0100

    Move MotionX specifics into a class

---
+++ motionx.php
@@ -0,0 +1,79 @@
+<?php
+
+class MotionX
+{
+    private $xml;
+
+    function __construct($xml) {
+        $this->setXml($xml);
+    }
+
+    public function setXml($xml) {
+        $this->xml = $xml;
+    }
+
+    public function getTrackName() {
+        return $this->xml->Document->name;
+    }
+
+    public function getTrackDescription() {
+        $this->xml->registerXPathNamespace('kml', 'http://earth.google.com/kml/2.2');
+        return $this->xml->xpath('//kml:description')[0];
+    }
+
+    public function getTrackDate() {
+        return strip_tags($this->getTrackDescription()->div->div->table->tr->td[1]->asXML());
+    }
+
+    public function getDistance() {
+        return strip_tags($this->getTrackDescription()->div->div->table->tr[1]->td[1]->asXML());
+    }
+
+    public function getElapsedTime() {
+        return strip_tags($this->getTrackDescription()->div->div->table->tr[2]->td[1]->asXML());
+    }
+
+    public function getAverageSpeed() {
+        return strip_tags($this->getTrackDescription()->div->div->table->tr[3]->td[1]->asXML());
+    }
+
+    public function getMaxSpeed() {
+        return strip_tags($this->getTrackDescription()->div->div->table->tr[4]->td[1]->asXML());
+    }
+
+    public function getAveragePace() {
+        return strip_tags($this->getTrackDescription()->div->div->table->tr[5]->td[1]->asXML());
+    }
+
+    public function getMinAltitude() {
+        return strip_tags($this->getTrackDescription()->div->div->table->tr[6]->td[1]->asXML());
+    }
+
+    public function getMaxAltitude() {
+        return strip_tags($this->getTrackDescription()->div->div->table->tr[7]->td[1]->asXML());
+    }
+
+    public function getStartTime() {
+        return strip_tags($this->getTrackDescription()->div->div->table[1]->tr[0]->td[1]->asXML());
+    }
+
+    public function getStartLatitude() {
+        return strip_tags($this->getTrackDescription()->div->div->table[1]->tr[2]->td[1]->asXML());
+    }
+
+    public function getStartLongitude() {
+        return strip_tags($this->getTrackDescription()->div->div->table[1]->tr[3]->td[1]->asXML());
+    }
+
+    public function getFinishTime() {
+        return strip_tags($this->getTrackDescription()->div->div->table[2]->tr[0]->td[1]->asXML());
+    }
+
+    public function getFinishLatitude() {
+        return strip_tags($this->getTrackDescription()->div->div->table[2]->tr[2]->td[1]->asXML());
+    }
+
+    public function getFinishLongitude() {
+        return strip_tags($this->getTrackDescription()->div->div->table[2]->tr[3]->td[1]->asXML());
+    }
+}
--- parser.php
+++ parser.php
@@ -1,24 +1,28 @@
 <?php
+require_once('motionx.php');
+
 $file = 'test/data/ride-one.kmz';
 
 $xml = getXml($file);
 
-echo getTrackName($xml) . PHP_EOL;
+$motionX = new MotionX($xml);
 
-echo getTrackDate($xml) . PHP_EOL;
-echo getDistance($xml) . PHP_EOL;
-echo getElapsedTime($xml) . PHP_EOL;
-echo getAverageSpeed($xml) . PHP_EOL;
-echo getMaxSpeed($xml) . PHP_EOL;
-echo getAveragePace($xml) . PHP_EOL;
-echo getMinAltitude($xml) . PHP_EOL;
-echo getMaxAltitude($xml) . PHP_EOL;
-echo getStartTime($xml) . PHP_EOL;
-echo getStartLatitude($xml) . PHP_EOL;
-echo getStartLongitude($xml) . PHP_EOL;
-echo getFinishTime($xml) . PHP_EOL;
-echo getFinishLatitude($xml) . PHP_EOL;
-echo getFinishLongitude($xml) . PHP_EOL;
+echo $motionX->getTrackName() . PHP_EOL;
+
+echo $motionX->getTrackDate() . PHP_EOL;
+echo $motionX->getDistance() . PHP_EOL;
+echo $motionX->getElapsedTime() . PHP_EOL;
+echo $motionX->getAverageSpeed() . PHP_EOL;
+echo $motionX->getMaxSpeed() . PHP_EOL;
+echo $motionX->getAveragePace() . PHP_EOL;
+echo $motionX->getMinAltitude() . PHP_EOL;
+echo $motionX->getMaxAltitude() . PHP_EOL;
+echo $motionX->getStartTime() . PHP_EOL;
+echo $motionX->getStartLatitude() . PHP_EOL;
+echo $motionX->getStartLongitude() . PHP_EOL;
+echo $motionX->getFinishTime() . PHP_EOL;
+echo $motionX->getFinishLatitude() . PHP_EOL;
+echo $motionX->getFinishLongitude() . PHP_EOL;
 
 function getXml($file) {
     $zip = new ZipArchive;
@@ -32,69 +36,4 @@ function getXml($file) {
     } else {
         echo "Failed to open {$file}";
     }
-}
-
-function getTrackName($xml) {
-    return $xml->Document->name;
-}
-
-function getTrackDescription($xml) {
-    $xml->registerXPathNamespace('kml', 'http://earth.google.com/kml/2.2');
-    return $xml->xpath('//kml:description')[0];
-}
-
-function getTrackDate($xml) {
-    return strip_tags(getTrackDescription($xml)->div->div->table->tr->td[1]->asXML());
-}
-
-function getDistance($xml) {
-    return strip_tags(getTrackDescription($xml)->div->div->table->tr[1]->td[1]->asXML());
-}
-
-function getElapsedTime($xml) {
-    return strip_tags(getTrackDescription($xml)->div->div->table->tr[2]->td[1]->asXML());
-}
-
-function getAverageSpeed($xml) {
-    return strip_tags(getTrackDescription($xml)->div->div->table->tr[3]->td[1]->asXML());
-}
-
-function getMaxSpeed($xml) {
-    return strip_tags(getTrackDescription($xml)->div->div->table->tr[4]->td[1]->asXML());
-}
-
-function getAveragePace($xml) {
-    return strip_tags(getTrackDescription($xml)->div->div->table->tr[5]->td[1]->asXML());
-}
-
-function getMinAltitude($xml) {
-    return strip_tags(getTrackDescription($xml)->div->div->table->tr[6]->td[1]->asXML());
-}
-
-function getMaxAltitude($xml) {
-    return strip_tags(getTrackDescription($xml)->div->div->table->tr[7]->td[1]->asXML());
-}
-
-function getStartTime($xml) {
-    return strip_tags(getTrackDescription($xml)->div->div->table[1]->tr[0]->td[1]->asXML());
-}
-
-function getStartLatitude($xml) {
-    return strip_tags(getTrackDescription($xml)->div->div->table[1]->tr[2]->td[1]->asXML());
-}
-
-function getStartLongitude($xml) {
-    return strip_tags(getTrackDescription($xml)->div->div->table[1]->tr[3]->td[1]->asXML());
-}
-
-function getFinishTime($xml) {
-    return strip_tags(getTrackDescription($xml)->div->div->table[2]->tr[0]->td[1]->asXML());
-}
-
-function getFinishLatitude($xml) {
-    return strip_tags(getTrackDescription($xml)->div->div->table[2]->tr[2]->td[1]->asXML());
-}
-
-function getFinishLongitude($xml) {
-    return strip_tags(getTrackDescription($xml)->div->div->table[2]->tr[3]->td[1]->asXML());
 }
