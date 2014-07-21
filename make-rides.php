dfdeebf13e9b3d1254edca112c76784697028138
Matthew Vivian <matthew@postino.co.uk>
2014-07-21 15:03:44 +0100

    Generate YAML collection properly

--- make-rides.php
+++ make-rides.php
@@ -1,15 +1,18 @@
 <?php
 require_once('Parser.php');
-require_once 'vendor/autoload.php';
+require_once('vendor/autoload.php');
 use Symfony\Component\Yaml\Yaml;
 
 $kmzFile = 'test/data/ride-one.kmz';
 $yamlFile = 'rides.yml';
 
-$parser = new Parser($kmzFile);
-var_dump($parser->getRide());
+$rides = array();
 
-$yaml = Yaml::dump(array(array('ride' => $parser->getRide())), 3);
-echo $yaml;
+echo "Parsing {$kmzFile}" . PHP_EOL;
+$parser = new Parser($kmzFile);
+$rides[] = $parser->getRide();
+
+$yaml = Yaml::dump($rides, 3);
+echo "Ride data appended to {$yamlFile}" . PHP_EOL;
 
 file_put_contents($yamlFile, $yaml, FILE_APPEND);
