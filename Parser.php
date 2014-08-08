f5e25ab7c2b9ea93de15481f1f4afd01b052e498
Matthew Vivian <matthew@postino.co.uk>
2014-08-08 19:05:13 +0100

    Add fence start/finish methods

--- Parser.php
+++ Parser.php
@@ -60,13 +60,54 @@ class Parser {
 
                 if ($long < $right && $long > $left
                     && $lat < $top && $lat > $bottom) {
-                    echo "In range: {$line}" . PHP_EOL;
+                    // echo "In range: {$line}" . PHP_EOL;
                 } else {
                     $geoFenced .= "{$long}, {$lat}, {$alt}" . PHP_EOL;
                 }
             }
         }
+
         $this->setCoordinates($geoFenced);
+        $this->fenceStart($top, $right, $bottom, $left);
+        $this->fenceFinish($top, $right, $bottom, $left);
+    }
+
+    private function fenceStart($top, $right, $bottom, $left) {
+        $startCoordinates = $this->getStartCoordinates();
+        if (!empty($startCoordinates)) {
+            list($long, $lat, $alt) = explode(',', $startCoordinates);
+
+            settype($long, "float");
+            settype($lat, "float");
+            settype($alt, "float");
+
+            if ($long < $right && $long > $left
+                && $lat < $top && $lat > $bottom) {
+                echo "Remove XML node $long $lat $alt" . PHP_EOL;
+            } else {
+                echo "Leave node as is";
+            }
+        }
+        // Save nodes
+    }
+    
+    private function fenceFinish($top, $right, $bottom, $left) {
+        $finishCoordinates = $this->getFinishCoordinates();
+        if (!empty($finishCoordinates)) {
+            list($long, $lat, $alt) = explode(',', $finishCoordinates);
+
+            settype($long, "float");
+            settype($lat, "float");
+            settype($alt, "float");
+
+            if ($long < $right && $long > $left
+                && $lat < $top && $lat > $bottom) {
+                echo "Remove XML node $long $lat $alt" . PHP_EOL;
+            } else {
+                echo "Leave node as is";
+            }
+        }
+        // Save nodes
     }
 
     private function getCoordinates() {
@@ -81,6 +122,18 @@ class Parser {
         $ls = $xml->getElementsByTagName('LineString')->item(0);
         $ls->getElementsByTagName('coordinates')->item(0)->nodeValue = $coordinates;
         $this->saveKmz();
+    }
+
+    private function getStartCoordinates() {
+        $xml = $this->getXml();
+        $point = $xml->getElementsByTagName('Point')->item(0);
+        return $point->getElementsByTagName('coordinates')->item(0)->nodeValue;
+    }
+
+    private function getFinishCoordinates() {
+        $xml = $this->getXml();
+        $point = $xml->getElementsByTagName('Point')->item(1);
+        return $point->getElementsByTagName('coordinates')->item(0)->nodeValue;
     }
 
     private function linesToArray($string) {
