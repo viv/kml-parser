7be0d1f0df7a1e4481d6635ad3d8148989c96ad3
Matthew Vivian <matthew@postino.co.uk>
2014-08-26 09:03:00 +0100

    Clean filename

--- make-rides.php
+++ make-rides.php
@@ -14,6 +14,12 @@ $kmzDir = new DirectoryIterator($kmzWatchDir);
 foreach ($kmzDir as $fileinfo) {
     if ($fileinfo->getExtension() === 'kmz') {
         echo "Processing " . $fileinfo->getFilename() . PHP_EOL;
+
+        echo "Cleaning filename" . PHP_EOL;
+        $badChars = array(" ");
+        $filename = str_replace($badChars, "-", $fileinfo->getFilename());
+
+        echo "Parsing" . PHP_EOL;
         $parser = new Parser($fileinfo->getRealPath());
 
         $top = 51.545425;
@@ -26,8 +32,9 @@ foreach ($kmzDir as $fileinfo) {
         $rides[] = $parser->getRide();
 
         // Move KMZ into folder for map to pick up
-        echo "Moving " . $fileinfo->getFilename() . " to {$kmzDestDir}" . PHP_EOL;
-        rename($fileinfo->getRealPath(), $kmzDestDir . $fileinfo->getFilename());
+        echo "Moving " . $fileinfo->getFilename() . " to {$kmzDestDir}{$filename}" . PHP_EOL;
+        rename($fileinfo->getRealPath(), $kmzDestDir . $filename);
+        echo PHP_EOL;
     }
 }
 
